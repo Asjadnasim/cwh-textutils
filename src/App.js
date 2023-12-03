a7ded31bb28745097ff219523878dcabb55b1712
Asjadnasim <asjadnasim@gmail.com>
2023-12-03 20:03:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [mode, setMode] = useState('light');
+	const [alert, setAlert] = useState(null);
+
+	const showAlert = (message, type) => {
+		setAlert({
+			message,
+			type,
+		});
+		setTimeout(() => {
+			setAlert(null);
+		}, 2000);
+	};
+
+	const toggleMode = () => {
+		if (mode === 'light') {
+			setMode('dark');
+			document.body.style.backgroundColor = '#042743';
+			document.body.style.color = 'white';
+			showAlert('Blue Mode has been enabled', 'success');
+			document.title = 'TextUtils - Dark Mode';
+		} else {
+			setMode('light');
+			document.body.style.backgroundColor = 'white';
+			document.body.style.color = 'black';
+			showAlert('Light Mode has been enabled', 'success');
+			document.title = 'TextUtils - Light Mode';
+		}
+	};
+
+	const changegreen = () => {
+		document.body.style.backgroundColor = '#35C649';
+		document.body.style.color = 'white';
+		showAlert('Color changed to green', 'success');
+		document.title = 'TextUtils - Green Mode';
+	};
+	const changeyellow = () => {
+		document.body.style.backgroundColor = '#CAB911';
+		document.body.style.color = 'white';
+		showAlert('Color changed to yellow', 'success');
+	};
+	const changered = () => {
+		document.body.style.backgroundColor = '#C15031';
+		document.body.style.color = 'white';
+		showAlert('Color changed to red', 'success');
+	};
+
+	return (
+		<>
+			<Router>
+				<Navbar
+					title='TextUtils'
+					aboutText='About Us'
+					mode={mode}
+					toggleMode={toggleMode}
+					changegreen={changegreen}
+					changered={changered}
+					changeyellow={changeyellow}
+				/>
+				<Alert alert={alert} />
+				<div className='container my-3'>
+					<Routes>
+						<Route exact path='/about' element={<About />}></Route>
+						<Route
+							exact
+							path='/'
+							element={
+								<TextForm
+									showAlert={showAlert}
+									heading='Enter your text to Analyze below'
+									mode={mode}
+								/>
+							}
+						></Route>
+					</Routes>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function TextForm(props) {
+	const handleUpClick = () => {
+		// console.log('Your uppercase clicked');
+		let newText = text.toUpperCase();
+		setText(newText);
+		props.showAlert('All letters has been capitalized.', 'success');
+	};
+
+	const handleLowClick = () => {
+		let newText = text.toLowerCase();
+		setText(newText);
+		props.showAlert('All letters has been lower case.', 'success');
+	};
+
+	const handleClearClick = () => {
+		let newText = '';
+		setText(newText);
+		props.showAlert('You cleared all the texts.', 'danger');
+	};
+
+	const handleCopyClick = () => {
+		navigator.clipboard.writeText(text);
+		props.showAlert('Copied to clipboard.', 'success');
+	};
+
+	const handleExtraSpaces = () => {
+		let newText = text.split(/[ ]+/);
+		setText(newText.join(' '));
+		props.showAlert('Remove all the extra spaces.', 'success');
+	};
+
+	const handleOnChange = (event) => {
+		// console.log('On changed');
+		setText(event.target.value);
+	};
+
+	const [text, setText] = useState('');
+	return (
+		<>
+			<div className='container'>
+				<h1>{props.heading}</h1>
+				<div className='mb-3'>
+					<textarea
+						className='form-control'
+						value={text}
+						onChange={handleOnChange}
+						placeholder='Enter text here...'
+						id='myBox'
+						rows='10'
+						style={{
+							backgroundColor: props.mode === 'dark' ? '#0b4a8b' : 'white',
+							color: props.mode === 'dark' ? 'white' : 'black',
+						}}
+					/>
+				</div>
+				<button className='btn btn-primary mx-1' onClick={handleUpClick}>
+					Convert to Uppercase
+				</button>
+				<button className='btn btn-primary mx-1' onClick={handleLowClick}>
+					Convert to Lowercase
+				</button>
+				<button className='btn btn-primary mx-1' onClick={handleClearClick}>
+					Clear Text
+				</button>
+				<button className='btn btn-primary mx-1' onClick={handleCopyClick}>
+					Copy Text
+				</button>
+				<button className='btn btn-primary mx-1' onClick={handleExtraSpaces}>
+					Remove extra spaces
+				</button>
+			</div>
+			<div className='contaier my-3'></div>
+			<h2>Your text summary</h2>
+			<p>
+				{text.split(' ').length - 1} words and {text.length} characters
+			</p>
+			<p>{0.008 * text.split(' ').length} minutes read</p>
+			<h2>Preview</h2>
+			<p>
+				{text.length > 0
+					? text
+					: 'Enter something in textbox to preview it here!'}
+			</p>
+		</>
+	);
+}
+
+TextForm.propTypes = { heading: PropTypes.string.isRequired };
